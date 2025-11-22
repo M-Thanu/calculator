5a5f91138a81561880a7ecdae70fd7965ba90fac
M-Thanu <mahanthanuja@gmail.com>
2025-11-22 21:42:46 +0530

    studying testNG

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,32 +1,55 @@
 package com.example;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 public class CalculatorTest{
-    @Test (groups = {"BasicOperations"})
-    public void addTest(){
-        Calculator c1 = new Calculator();
 
-        Assert.assertEquals(c1.add(7,4),11);
+    @BeforeClass
+    public void initial(){
+        System.out.println("Initiatee Tests");
     }
 
-    @Test (groups = {"BasicOperations"})
+    @AfterClass
+    public void last(){
+        System.out.println("End of Tests");
+    }
+
+    @DataProvider(name="dforAdd")
+    public Object [][] dforAdd(){
+        return new Object[][]{
+            {1,2,3},
+            {2,3,5},
+            {3,3,6}
+        };
+    }
+
+    @Test (groups = {"BasicOperations"},dataProvider="dforAdd")
+    public void addTest(int a, int b, int exp){
+        Calculator c1 = new Calculator();
+
+        Assert.assertEquals(c1.add(a,b),exp);
+    }
+
+    @Test (groups = {"BasicOperations"}, priority=2)
     public void subtractTest(){
         Calculator c1 = new Calculator();
 
         Assert.assertEquals(c1.subtract(7,4),3);
     }
 
-    @Test (groups = {"AdvancedOperations"})
+    @Test (groups = {"AdvancedOperations"}, priority=1)
     public void multiplyTest(){
         Calculator c1 = new Calculator();
 
         Assert.assertEquals(c1.multiply(7,4),28);
     }
 
-    @Test (groups = {"AdvancedOperations"})
+    @Test (groups = {"AdvancedOperations"}, dependsOnMethods={"multiplyTest"})
     public void divideTest(){
         Calculator c1 = new Calculator();
 
@@ -35,18 +58,6 @@ public class CalculatorTest{
     
 }
 
-/*package com.example;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class CalculatorTest {
-    @Test
-    public void addTest(){
-        Calculator c1 = new Calculator();
-
-        Assert.assertEquals(4, c1.add(3,1));
-    }
-}*/
 
 
